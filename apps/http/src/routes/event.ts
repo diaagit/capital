b2aa0505ff9846ba90a4a913676465e43abc2d16
vedang29 <vedang294@gmail.com>
2025-08-28 12:46:55 +0530

    Added Routing files

---
+++ apps/http/src/routes/event.ts
@@ -0,0 +1,10 @@
+// Events Management Routes
+/*
+| **Module** | **API Endpoint** | **Method** | **Role** | **Description** |
+|  | `/events` | POST | Admin | Create new event |
+|  | `/events/:id` | PUT | Admin | Update event details |
+|  | `/events/:id` | DELETE | Admin | Delete/cancel event |
+|  | `/events` | GET | User/Admin | List all events (with filters) |
+|  | `/events/:id` | GET | User/Admin | Get event details |
+*/
+
