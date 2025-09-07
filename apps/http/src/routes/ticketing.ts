74854561d6ea617f206624dda3839a43e94a8f8f
ronakmaheshwari <ronakmaheshwari077@gmail.com>
2025-09-07 18:36:30 +0530

    Ticketing Logic fixed

--- apps/http/src/routes/ticketing.ts
+++ apps/http/src/routes/ticketing.ts
@@ -1,11 +1,12 @@
-// Ticketing routes
-
 import crypto from "node:crypto";
-import db from "@repo/db";
+import db, { type Prisma } from "@repo/db";
+import { createSignedTicket } from "@repo/keygen";
+import { type TicketPurchaseResponseType, TicketPurchaseSchema } from "@repo/types";
 import { createClient } from "@supabase/supabase-js";
 import axios from "axios";
 import express, { type Request, type Response, type Router } from "express";
 import QRCode from "qrcode";
+import userMiddleware from "../middleware";
 import { decrypt, signMessage } from "../utils/encrypter";
 import { formatDateToIST, sendTicketEmail } from "../utils/sendTicketEmail";
 
@@ -15,14 +16,196 @@ const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+//Not needed will make it from ZOD
 interface PurchaseTicketPayload {
-    userId: string;
+    userId?: string;
     eventSlotId: string;
     quantity: number;
     cardNumber: string;
 }
 
-ticketRouter.post("/purchase", async (req: Request, res: Response) => {
+export interface TicketPurchaseErrorResponse {
+    message: string;
+    errors?: unknown;
+    error: string;
+}
+
+//Ronak Here
+ticketRouter.post(
+    "/purchase",
+    userMiddleware,
+    async (
+        req: Request,
+        res: Response<TicketPurchaseResponseType | TicketPurchaseErrorResponse>,
+    ) => {
+        try {
+            const userId = req.userId;
+            const parsedData = TicketPurchaseSchema.safeParse(req.body);
+            if (!parsedData.success) {
+                return res.status(400).json({
+                    errors: parsedData.error.issues,
+                    message: "userId, eventSlotId, quantity, and cardNumber are required",
+                });
+            }
+            const { token, eventSlotId, quantity, cardNumber } = parsedData.data;
+
+            if (!userId || !eventSlotId || !quantity || !cardNumber) {
+                return res.status(400).json({
+                    message: "userId, eventSlotId, quantity, and cardNumber are required",
+                });
+            }
+
+            const [user, eventDetail, eventSlotDetail, CheckCard] = await Promise.all([
+                db.user.findUnique({
+                    where: {
+                        id: userId,
+                    },
+                }),
+                await db.event.findFirst({
+                    include: {
+                        organiser: true,
+                    },
+                    where: {
+                        slots: {
+                            some: {
+                                id: eventSlotId,
+                            },
+                        },
+                    },
+                }),
+                db.eventSlot.findUnique({
+                    include: {
+                        event: true,
+                        tickets: true,
+                    },
+                    where: {
+                        id: eventSlotId,
+                    },
+                }),
+                db.card.findUnique({
+                    where: {
+                        card_number: cardNumber,
+                    },
+                }),
+            ]);
+
+            if (
+                !user ||
+                !eventSlotDetail ||
+                eventDetail.status === "cancelled" ||
+                !CheckCard ||
+                CheckCard.userId !== userId
+            ) {
+                return res.status(404).json({
+                    message: "User not found or Eventslot is invalid",
+                });
+            }
+
+            if (eventSlotDetail.capacity < eventSlotDetail.tickets.length + quantity) {
+                return res.status(400).json({
+                    message: "Not enough capacity in this slot",
+                });
+            }
+            // const ticketPrice = (eventSlot.tickets as any).price ?? 100;    We dont have it in DB So why have you added it @vedang
+            // const totalAmount = ticketPrice * quantity;
+            const totalAmount = 3000; //remove this with real logic
+
+            const ticketPayload = {
+                email: user.email,
+                eventEndTime: new Date(eventSlotDetail.end_time).toISOString(),
+                eventId: eventDetail.id,
+                eventLocation: eventDetail.location_name,
+                eventSlotId: eventSlotDetail.id,
+                eventStartTime: new Date(eventSlotDetail.start_time).toISOString(),
+                eventTitle: eventDetail.title,
+                firstName: user.first_name,
+                issuedAt: new Date().toISOString(),
+                lastName: user.last_name,
+                quantity,
+                ticketId: token,
+                totalAmount,
+                transactionToken: token,
+            };
+            const decryptedPrivateKey = decrypt(user.encrypted_private_key);
+            const signedTicketPayload = createSignedTicket(ticketPayload, decryptedPrivateKey);
+
+            const qrBuffer = await QRCode.toBuffer(
+                JSON.stringify({
+                    signedTicketPayload,
+                }),
+            );
+            const fileName = `tickets/${userId}-${Date.now()}.png`;
+            const { error: uploadError } = await supabase.storage
+                .from("tickets")
+                .upload(fileName, qrBuffer, {
+                    contentType: "image/png",
+                });
+            if (uploadError) throw uploadError;
+
+            const { data: publicUrlData } = supabase.storage.from("tickets").getPublicUrl(fileName);
+            const qrCodeUrl = publicUrlData.publicUrl;
+
+            const _purchasedTicket = await db.$transaction(async (tx: Prisma.TransactionClient) => {
+                const TicketCreate = await tx.ticket.create({
+                    data: {
+                        eventSlotId: eventSlotDetail.id,
+                        qr_code_data: qrCodeUrl,
+                        signature: (await signedTicketPayload).signature,
+                        userId,
+                    },
+                });
+
+                await tx.transaction.create({
+                    data: {
+                        amount: 3000, //Create a logic for me
+                        bank_name: CheckCard.bank_name,
+                        cardId: CheckCard.id,
+                        description: `Tickets for ${user.email} was taken place`,
+                        ticket_count: quantity,
+                        ticketId: TicketCreate.id,
+                        token,
+                        type: "PURCHASE",
+                        userId,
+                    },
+                });
+            });
+
+            //add ticketId of purchased in email as TXN<ticketId> or const purchasedTicketId = "TXN"+token;
+            await sendTicketEmail({
+                attendeeName: `${user.first_name?.trim()} ${user.last_name?.trim()}`,
+                baseAmount: totalAmount,
+                bookingDateTime: new Date().toISOString(),
+                convenienceFee: 0,
+                email: user.email,
+                eventDate: new Date(eventSlotDetail.start_time).toISOString(),
+                eventLocation: eventDetail.location_name,
+                eventTime: `${eventSlotDetail.start_time} - ${eventSlotDetail.end_time}`,
+                eventTitle: eventDetail.title,
+                gstAmount: 0,
+                gstRate: 0,
+                organiser: `${eventDetail.organiser.first_name}`, //Why this field Dont expose the organiserId Ever
+                paymentType: "Card",
+                qrCodeUrl,
+                quantity,
+                seats: `General Admission x${quantity}`,
+                totalPaid: totalAmount,
+            });
+
+            res.status(200).json({
+                message: "Tickets purchased successfully",
+                ticketURL: qrCodeUrl,
+            });
+        } catch (error) {
+            console.error("Internal error record", error);
+            return res.status(500).json({
+                error: "Internal error occured",
+                message: "Internal error occured",
+            });
+        }
+    },
+);
+
+ticketRouter.post("/purchase", userMiddleware, async (req: Request, res: Response) => {
     try {
         const payload: PurchaseTicketPayload = req.body;
         const { userId, eventSlotId, quantity, cardNumber } = payload;
@@ -81,7 +264,8 @@ ticketRouter.post("/purchase", async (req: Request, res: Response) => {
             token,
         });
 
-        // 5️⃣ Prepare event slot times in IST
+        //Why are you doing this ??
+        // 5️⃣ Prepare event slot times in IST?
         const eventDateIST = formatDateToIST(slot.start_time).split(",")[0];
         const startTimeIST = formatDateToIST(slot.start_time).split(",")[1].trim();
         const endTimeIST = formatDateToIST(slot.end_time).split(",")[1].trim();
