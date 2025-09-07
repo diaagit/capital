53fc21939f7886c09e635f0bb87c43ac608b2fdc
ronakmaheshwari <ronakmaheshwari077@gmail.com>
2025-09-08 02:10:47 +0530

    Added Organiser Route

--- apps/http/src/routes/index.ts
+++ apps/http/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express, { type Router } from "express";
 import eventRouter from "./event";
+import organiserRouter from "./organiser";
 import userRouter from "./user";
 
 const router: Router = express.Router();
 
 router.use("/user", userRouter);
 router.use("/events", eventRouter);
+router.use("organiser", organiserRouter);
 
 export default router;
