4a30384fb660d3dd4c3117ba5a85279c317c58bc
vedang29 <vedang294@gmail.com>
2025-10-08 08:10:57 +0530

    Fixed the sidebar issue

--- apps/web/app/newdashboard/layout.tsx
+++ apps/web/app/newdashboard/layout.tsx
@@ -11,8 +11,28 @@ export default function DashboardLayout({
       <Sidebar />
       <div className="w-full">
         <Navbar />
-        <div className="">{children}</div>
+        <div>{children}</div>
       </div>
     </div>
   );
 }
+
+
+// import Navbar from "@/components/new custom/Navbar";
+// import Sidebar from "@/components/new custom/Sidebar";
+
+// export default function DashboardLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <div className="max-w-7xl flex mx-auto">
+//       <Sidebar />
+//       <div className="w-full">
+//         <Navbar />
+//         <div className="">{children}</div>
+//       </div>
+//     </div>
+//   );
+// }
